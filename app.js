1e18d56c3f7b425a730ace1fd09c547922959c05
CPalileo <cpalileo@gmail.com>
2022-02-17 16:39:58 -0800

    generate webpage

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  const generatePage = require("./src/page-template.js");
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
